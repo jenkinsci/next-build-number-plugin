70ce155ed278873aefb80062c342188fc5ca549a
alexander.komarov <alexander.komarov@nytimes.com>
2015-08-21 17:11:44 -0400

    Updated to use TriggerContext and it is now working correctly.

---
+++ src/main/java/org/jvnet/hudson/plugins/nextbuildnumber/NextBuildNumberTrigger.java
@@ -0,0 +1,64 @@
+package org.jvnet.hudson.plugins.nextbuildnumber;
+
+import hudson.Extension;
+import hudson.model.Item;
+import hudson.model.Job;
+import hudson.model.JobPropertyDescriptor;
+import hudson.tasks.BuildWrapper;
+import hudson.triggers.Trigger;
+import hudson.triggers.TriggerDescriptor;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Set the nextBuildNumber when the trigger is first started
+ *
+ */
+public class NextBuildNumberTrigger extends Trigger {
+    private static final Logger LOG = Logger.getLogger(NextBuildNumberTrigger.class.getName());
+
+    private final int nextBuildNumber;
+
+    public NextBuildNumberTrigger(int nextBuildNumber) {
+        this.nextBuildNumber = nextBuildNumber;
+    }
+
+    @Override
+    public void start(Item project, boolean newInstance) {
+        if (!newInstance) {
+            return; //this trigger only makes sense in the context of a Job DSL run, beyond that it is useless.
+        }
+        if (project instanceof Job) {
+            LOG.info(String.format("Setting nextBuildNumber=%d for project %s", nextBuildNumber, project.getFullDisplayName()));
+            try {
+                ((Job) project).updateNextBuildNumber(nextBuildNumber);
+            } catch (IOException e) {
+                LOG.log(Level.WARNING, "Error setting next build number for build " + project.getFullDisplayName(), e);
+            }
+        }
+        super.start(project, newInstance);
+    }
+
+    /**
+     * Registers {@link NextBuildNumberTrigger} as a {@link Trigger}.
+     */
+    @Extension
+    public static final class DescriptorImpl extends TriggerDescriptor {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String getDisplayName() {
+            return Messages.DisplayName();
+        }
+
+        @Override
+        public boolean isApplicable(Item item) {
+            return item instanceof Job;
+        }
+
+    }
+}
