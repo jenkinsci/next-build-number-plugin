78a0f07de60678b0d8813a2bdca48433043a1ed0
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-04-07 16:02:41 +0200

    Migrate tests to JUnit5
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/org/jvnet/hudson/plugins/nextbuildnumber/JobTestBase.java
+++ src/test/java/org/jvnet/hudson/plugins/nextbuildnumber/JobTestBase.java
@@ -3,30 +3,33 @@ package org.jvnet.hudson.plugins.nextbuildnumber;
 import hudson.model.Job;
 import hudson.security.AuthorizationStrategy;
 import org.htmlunit.html.HtmlForm;
-import org.junit.Before;
-import org.junit.Rule;
+import org.junit.jupiter.api.BeforeEach;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class JobTestBase {
+@WithJenkins
+class JobTestBase {
 
-    @Rule
-    public JenkinsRule jenkins = new JenkinsRule();
+    protected JenkinsRule jenkins;
 
-    @Before
-    public void setUp() {
+    @BeforeEach
+    void setUp(JenkinsRule rule) {
+        jenkins = rule;
         jenkins.getInstance().setAuthorizationStrategy(new AuthorizationStrategy.Unsecured());
     }
 
     @SuppressWarnings("rawtypes")
     protected void performNextBuildNumberChange(Job project, String currenNumber, String changeToNumber) throws Exception {
-        HtmlForm form = jenkins.createWebClient().getPage(project, "nextbuildnumber").getFormByName("nextbuildnumber");
-        assertEquals(currenNumber, form.getInputByName("nextBuildNumber").getDefaultValue());
-        form.getInputByName("nextBuildNumber").setValue(changeToNumber);
-        jenkins.submit(form);
+        try (JenkinsRule.WebClient client = jenkins.createWebClient()) {
+            HtmlForm form = client.getPage(project, "nextbuildnumber").getFormByName("nextbuildnumber");
+            assertEquals(currenNumber, form.getInputByName("nextBuildNumber").getDefaultValue());
+            form.getInputByName("nextBuildNumber").setValue(changeToNumber);
+            jenkins.submit(form);
 
-        form = jenkins.createWebClient().getPage(project, "nextbuildnumber").getFormByName("nextbuildnumber");
-        assertEquals(changeToNumber, form.getInputByName("nextBuildNumber").getDefaultValue());
+            form = client.getPage(project, "nextbuildnumber").getFormByName("nextbuildnumber");
+            assertEquals(changeToNumber, form.getInputByName("nextBuildNumber").getDefaultValue());
+        }
     }
 }
